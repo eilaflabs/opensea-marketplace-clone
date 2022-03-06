96c6ef3408bb4ff37e1dd021a594b5459dd5a7c5
Ameer Hamza <AmeerhamzaFalconItConsultnt>
2022-03-06 20:17:54 +0500

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+// Chains ID 4 is Rinkeby network
+const supportesChainsIds = [4];
+// the "injected" connector is web 3 connection method used by metamask
+const connectors = { injected: {} };
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportesChainsIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />;
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ static/dummyEvents.js
@@ -0,0 +1,18 @@
+export const dummyEvents = [
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+  { price: 18, from: "0xHAZAoP", to: "0xHAZA GIGA", date: "1 month ago" },
+];
